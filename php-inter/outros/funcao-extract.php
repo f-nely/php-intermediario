577c49705d46a5796c9ee229a2ceef28c7a726e8
nely de faria <nelly.nf54@gmail.com>
2021-01-04 16:19:24 -0300

    Usando a função extract com parâmetros

--- php-inter/outros/funcao-extract.php
+++ php-inter/outros/funcao-extract.php
@@ -9,11 +9,16 @@ $dados = [
     'site' => 'http://www.treinaweb.com.br'
 ];
 
-// Extrai os dados do array para variáveis
-extract($dados, EXTR_SKIP);
+/*
+ * extract — Importa variáveis para a tabela de símbolos a partir de um array.
+ * extract() verifica cada chave e vê se ela tem um nome válido nome de variável.
+ * Também verifica por colisões com já existente variáveis na tabela de símbolo.
+ */
+extract($dados, EXTR_PREFIX_ALL, 'extr1');
 
 // Acessa as variáveis criadas dinamicamente
-echo $nome . "<br>";
-echo $email . "<br>";
-echo $telefone . "<br>";
-echo $site;
+echo $nome . PHP_EOL;
+echo $extr1_nome . PHP_EOL;
+echo $extr1_email . PHP_EOL;
+echo $extr1_telefone . PHP_EOL;
+echo $extr1_site;
