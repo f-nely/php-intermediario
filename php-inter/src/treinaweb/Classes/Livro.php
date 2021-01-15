6e31dfbd5da2739e02e2f33c458327b93ba60a6d
nely de faria <nelly.nf54@gmail.com>
2021-01-15 18:11:54 -0300

    Cria método cadastrar e mostrar

--- php-inter/src/treinaweb/Classes/Livro.php
+++ php-inter/src/treinaweb/Classes/Livro.php
@@ -3,5 +3,21 @@
 namespace Treinaweb\Classes;
 class Livro
 {
+    public int $codigo;
+    public string $titulo;
+    public string $autor;
+    public float $valor;
 
+    public function cadastrar():void
+    {
+        echo "Cadastra $this->titulo no banco de dados<br>";
+    }
+
+    public function mostrar(): void
+    {
+        echo "Código: $this->codigo <br>";
+        echo "Título: $this->titulo <br>";
+        echo "Autor: $this->autor <br>";
+        echo "Valor: $this->valor <br>";
+    }
 }
