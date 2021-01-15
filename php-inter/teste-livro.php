a16d9dfbc18651fb370b2726037df6a6063b788c
nely de faria <nelly.nf54@gmail.com>
2021-01-15 18:12:36 -0300

    Utiliza métodos

--- php-inter/teste-livro.php
+++ php-inter/teste-livro.php
@@ -4,6 +4,14 @@ use Treinaweb\Classes\Livro;
 
 require_once 'vendor/autoload.php';
 
-$livro = new Livro();
+$livro1 = new Livro();
 
-var_dump($livro);
+$livro1->codigo = 8;
+$livro1->titulo = "Senhor dos Anéis";
+$livro1->autor = "JFF Tolkien";
+$livro1->valor = 99.90;
+
+$livro1->cadastrar();
+$livro1->mostrar();
+
+var_dump($livro1);
