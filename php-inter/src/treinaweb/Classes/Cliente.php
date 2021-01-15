72622ed1c77376b16b38c16f849a25d1fb2ad8a5
nely de faria <nelly.nf54@gmail.com>
2021-01-15 19:06:31 -0300

    Refatora código

--- php-inter/src/treinaweb/Classes/Cliente.php
+++ php-inter/src/treinaweb/Classes/Cliente.php
@@ -4,33 +4,7 @@
 namespace Treinaweb\Classes;
 
 
-class Cliente
+class Cliente extends Pessoa
 {
-    private string $nome;
-    private string $email;
 
-    public function defineNome($nome): void
-    {
-        $this->nome = $nome;
-    }
-
-    public function defineEmail($email)
-    {
-        $this->validarEmail($email);
-    }
-
-    public function validarEmail($email): bool
-    {
-        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $this->email = $email;
-            return true;
-        }
-
-        exit("Email inválido");
-    }
-
-    public function recuperaEmail(): string
-    {
-        return $this->email;
-    }
 }
