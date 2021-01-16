eba4a2835fa5a1524b8bc37d0c4ffba107fd9050
nely de faria <nelly.nf54@gmail.com>
2021-01-16 12:31:58 -0300

    Cria método abstrato defineVida e construtor

--- php-inter/src/treinaweb/Classes/Ator.php
+++ php-inter/src/treinaweb/Classes/Ator.php
@@ -6,7 +6,14 @@ namespace Treinaweb\Classes;
 
 abstract class Ator
 {
-    protected int $vida = 100;
+    protected int $vida;
+
+    public function __construct()
+    {
+        $this->defineVida();
+    }
+
+    protected abstract function defineVida();
 
     public function tomarDano(): void
     {
