93e82de6e94e84d6819b36458544334d0a940cf6
nely de faria <nelly.nf54@gmail.com>
2021-01-16 11:52:57 -0300

    Sobrescrita do método exibe

--- php-inter/src/treinaweb/Classes/Cliente.php
+++ php-inter/src/treinaweb/Classes/Cliente.php
@@ -6,5 +6,19 @@ namespace Treinaweb\Classes;
 
 class Cliente extends Pessoa
 {
+    private string $cpf;
 
+    public function defineCpf(string $cpf): void
+    {
+        $this->cpf = $cpf;
+    }
+
+    public function exibe()
+    {
+        echo "Dados do Cliente<br>";
+        echo "Código: $this->codigo <br>";
+        echo "Nome: $this->nome <br>";
+        echo "Email: $this->email <br>";
+        echo "CPF: $this->cpf <br>";
+    }
 }
--- php-inter/src/treinaweb/Classes/Fornecedor.php
+++ php-inter/src/treinaweb/Classes/Fornecedor.php
@@ -6,5 +6,19 @@ namespace Treinaweb\Classes;
 
 class Fornecedor extends Pessoa
 {
+    private string $cnpj;
 
+    public function defineCnpj(string $cnpj): void
+    {
+        $this->cnpj = $cnpj;
+    }
+
+    public function exibe()
+    {
+        echo "<br>Dados do Fornecedor<br>";
+        echo "Código: $this->codigo <br>";
+        echo "Nome: $this->nome <br>";
+        echo "Email: $this->email <br>";
+        echo "CNPJ: $this->cnpj <br>";
+    }
 }
